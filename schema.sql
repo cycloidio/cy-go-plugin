4dc71fa5bcc8fed7a8c652b3e3254bc1cc375316
Ivan Jozic <ivan.jozic@cycloid.io>
2026-04-16 16:00:32 +0200

    schema: modified to match sentry plugin schema

--- schema.sql
+++ schema.sql
@@ -1,16 +1,32 @@
-CREATE TABLE IF NOT EXISTS authors (
-		id INTEGER PRIMARY KEY,
-		name TEXT
+CREATE TABLE IF NOT EXISTS organizations (
+	id   TEXT PRIMARY KEY,
+	name TEXT,
+	slug TEXT
 );
 
-CREATE TABLE IF NOT EXISTS quotes (
-		id INTEGER PRIMARY KEY,
-		message TEXT,
-		author_id INT UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS projects (
+	id              TEXT PRIMARY KEY,
+	name            TEXT,
+	slug            TEXT,
+	status          TEXT,
+	organization_id TEXT NOT NULL,
+	CONSTRAINT fk__projects__organizations
+		FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE
+);
 
-
-		CONSTRAINT fk__quotes__authors
-			FOREIGN KEY (author_id) REFERENCES authors (id)
-			ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS issues (
+	id         TEXT PRIMARY KEY,
+	title      TEXT,
+	permalink  TEXT,
+	has_seen   BOOLEAN,
+	first_seen DATETIME,
+	last_seen  DATETIME,
+	user_count INT,
+	level      TEXT,
+	status     TEXT,
+	type       TEXT,
+	project_id TEXT NOT NULL,
+	CONSTRAINT fk__issues__projects
+		FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE CASCADE
 );
 
